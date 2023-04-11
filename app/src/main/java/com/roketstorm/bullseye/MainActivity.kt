683ce073f70dee7157d772a9d6d828999f312c4b
Artur Shamsutdinov <a.shamsutdinov@icloud.com>
2023-04-11 14:43:40 +0500

    Added point system

--- app/src/main/java/com/roketstorm/bullseye/MainActivity.kt
+++ app/src/main/java/com/roketstorm/bullseye/MainActivity.kt
@@ -2,10 +2,71 @@ package com.roketstorm.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AlertDialog
+import com.roketstorm.bullseye.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var sliderValue = 0
+    private var targetValue = Random.nextInt(1, 100)
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.targetTextView.text = targetValue.toString()
+
+        binding.hitMeButton.setOnClickListener {
+            Log.i("Button click event", "You clicked Hit Me button")
+            showResult()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sliderValue = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+    }
+
+    private fun pointsForCurrentRound(): Int {
+        val maxScore = 100
+
+        val difference = if (sliderValue > targetValue) {
+            sliderValue - targetValue
+        } else  if (targetValue > sliderValue) {
+            targetValue - sliderValue
+        } else {
+            0
+        }
+        return maxScore - difference
+    }
+
+    private fun showResult() {
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage =
+            getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
+
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+
+        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
